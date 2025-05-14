6e837c3276f52725dd6be9e8509469499359c2a5
M4LDIT4 <96477682+imtheclown@users.noreply.github.com>
2025-05-14 13:31:49 +0800

    commit to save

--- lib/features/animal_database/presentation/add_animal.dart
+++ lib/features/animal_database/presentation/add_animal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app_template/core/constants/sizes.dart';
 import 'package:mobile_app_template/core/widgets/navigation/generic_appbar.dart';
 
 class AddAnimalScreeen extends StatelessWidget {
@@ -8,6 +9,26 @@ class AddAnimalScreeen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: GenericAppbar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: TSizes.defaultScreenPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  elevation: 0,
+                  child: Padding(
+                    padding: EdgeInsets.all(TSizes.paddingmd),
+                    child: Text("This is a card"),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
