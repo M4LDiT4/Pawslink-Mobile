76c6bd81060dacf96f6d905372cf218f805eda29
M4LDIT4 <calugas.jonathan.18@gmail.com>
2025-06-02 18:09:56 +0800

    commit to save

---
+++ lib/services/api/animal_api.dart
@@ -0,0 +1,12 @@
+
+class AnimalApi {
+  final String baseUrl = "";
+  AnimalApi._();
+
+  static Future<void> addAnimal() async {
+    //create a map from the argument that will be used as the payload for the request
+    //create headers for this request
+    //execute request
+    //return a response    
+  }
+}
